e89251426e9746c0d3e3c9dc540c39b862dd3f47
Abdelrhman Ahmed <mrblackvanta@gmail.com>
2022-10-03 14:31:58 +0200

    Update Signature.js

--- src/Signature.js
+++ src/Signature.js
@@ -1,16 +1,18 @@
 import "./Signature.scss";
 function Signature() {
   return (
-    <footer class="attribution" role="contentinfo">
-      Challenge by
-      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-        Frontend Mentor
-      </a>
-      . Coded by
-      <a href="https://github.com/MrBlackvanta" target="_blank">
-        MrBlackvanta
-      </a>
-      .
+    <footer class="attribution">
+      <p>
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by
+        <a href="https://github.com/MrBlackvanta" target="_blank">
+          MrBlackvanta
+        </a>
+        .
+      </p>
     </footer>
   );
 }
